530b143d7572cddad970cec26d8f33e8e0bbda15
bgsukdev <bgsukinfo@gmail.com>
2026-04-30 19:49:49 +0100

    Release - Faiyaz - v1.5 - Bind production server to all interfaces

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -6,7 +6,8 @@ module.exports = {
       script: "server.js",
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
+        PORT: "3000",
+        HOSTNAME: "0.0.0.0",
       },
     },
   ],
